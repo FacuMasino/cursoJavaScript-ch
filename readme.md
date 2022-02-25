097df85540ac1a9f46123b0b3bfd78a53a49dde5
Facu Masino <facujmasino@gmail.com>
2022-02-24 23:26:20 -0300

    first commit

---
+++ readme.md
@@ -0,0 +1,8 @@
+
+# ![js](img/js.png) Curso JavaScript
+## Clase 03 - Ciclos y Bucles
+### Desafío Entregable
+> Tomando como base los ejemplos anteriores de la estructura for, while y do...while, crear un algoritmo que repita un bloque de instrucciones.
+
+- Mostrar en consola si el alumno esta aprobado o no
+- Pedir un número por prompt, repetir la salida del mensaje “Hola” la cantidad de veces ingresada.
---
+++ src/main.js
@@ -0,0 +1,22 @@
+// Mostrar en consola si el alumno está aprobado o no
+const proyectosFinales = [10, 6, 3, 6, 8, 2];
+
+console.log('Resultados de proyectos finales:');
+
+for (i = 0; i <= 5; i++){
+    if(proyectosFinales[i] < 6){
+        console.log(`ID de Alumno: ${i}\nCalificación: ${proyectosFinales[i]}\nEstado: %cdesaprobado :(`, 'color: red;');
+    } else {
+        console.log(`ID de Alumno: ${i} \nCalificación: ${proyectosFinales[i]} \nEstado: %caprobado! :)`,'color: green;');
+    }
+}
+
+// Pedir un número por prompt, repetir la salida del mensaje “Hola” la cantidad de veces ingresada.
+
+//const nVeces = Number(prompt('Ingresar un número:'));
+i = 0;
+
+while(i < nVeces){
+    console.log('Hola');
+    i++;
+}
